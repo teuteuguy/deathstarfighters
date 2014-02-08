d53b7cf0078f5b2aa2bcca6957041810a17265b1
teuteuguy <pub@lalatte.com>
2014-02-08 16:21:00 +0800

    Added third view controller and files

---
+++ deathstarfighers/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  deathstarfighers
+//
+//  Created by Timothee Cruse on 08/02/14.
+//  Copyright (c) 2014 TC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UITableViewController
+
+@end
